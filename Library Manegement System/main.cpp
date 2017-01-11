ea7b9c2a372d83823aabf4c9a99ef9da08c8ea00
Riya Walia <riyawalia12@gmail.com>
2017-01-11 18:54:16 -0500

    Initial Commit

---
+++ Library Manegement System/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Library Manegement System
+//
+//  Created by Riya Walia on 2017-01-11.
+//  Copyright © 2017 Riya Walia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
